f4d701284b0f96a2b541b885fb21e1bba0ae35de
Ryan Anthony Royle <ryanroyle87@icloud.com>
2024-01-24 22:03:33 -0600

    updated javascript dynamic

--- app.js
+++ app.js
@@ -1,38 +1,33 @@
 let inputArray = [];
 
 function madInput() {
-  let adjective = document.getElementById("adjective");
-  let noun = document.getElementById("noun");
-  let verb = document.getElementById("verb");
+  inputArray = [];
+  let word1 = document.getElementById("adjective").value;
+  let word2 = document.getElementById("noun").value;
+  let word3 = document.getElementById("verb").value;
 
-  let value1 = adjective.value;
-  let value2 = noun.value;
-  let value3 = verb.value;
+  if (word1.length > 0 && word2.length > 0 && word3.length > 0) {
+    inputArray.push(word1, word2, word3);
+    generateMadLib(inputArray);
+  } else {
+    alert("Your LIB isn't MAD enough!");
+  }
 
-  inputArray.push(value1, value2, value3);
-  document.getElementById("word1").innerHTML = value1;
-  document.getElementById("word2").innerHTML = value2;
-  document.getElementById("word3").innerHTML = value3;
+  //toggleDivOn();
+}
 
-  toggleDivOn();
+function generateMadLib(inputArray) {
+  if (inputArray.length > 0) {
+    let madLibString =
+      "I went to the {0} store to buy some {1} so I could {2} with it";
+    for (let i = 0; i < inputArray.length; i++) {
+      madLibString = madLibString.replace("{" + i + "}", inputArray[i]);
+    }
+    document.getElementById("final").innerHTML = madLibString;
+    console.log(inputArray);
+  }
 }
 
 function clearArray() {
   inputArray.length = 0;
-  document.getElementById("word1").innerHTML = "";
-  document.getElementById("word2").innerHTML = "";
-  document.getElementById("word3").innerHTML = "";
-  toggleDivOff();
-}
-
-function toggleDivOn() {
-  let div = document.getElementById("final");
-  if ((inputArray.length = 0)) {
-    div.style.display = "none";
-  } else div.style.display = div.style.display == "none" ? "block" : "block";
-}
-
-function toggleDivOff() {
-  let div = document.getElementById("final");
-  div.style.display = div.style.display == "block" ? "none" : "none";
 }
